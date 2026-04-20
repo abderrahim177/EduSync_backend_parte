c8081c8361b8e8451ca4fa54fb5d21f8468794a7
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-04-20 17:31:05 +0100

    Add initial content for all pages

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <header class="flex justify-between items-center h-[70px] px-[30px] bg-[#252543] text-white">
+        <h1 class="text-[30px] font-bold">Logo</h1>
+        <nav class="flex gap-[30px] items-center">
+            <a class="font-semibold" href="../public/index.php">Home</a>
+            <a class="bg-white text-[#06060c] font-semibold px-5 py-2 rounded-2xl shadow-2xl" href="../public/login.php">Sing in</a>
+            <a class="bg-[#06060c] px-5 py-2 font-semibold rounded-2xl" href="../public/register.php">Sing Up</a>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+include('../includes/header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex text-center flex-col h-[100vh] justify-center items-center gap-4">
+        <h1 class="text-4xl font-bold">Welcome ✋</h1>
+        <button class="bg-blue-600 px-6 py-3 rounded-lg outline-none text-white text-xl transition duration-300 hover:shadow-xl hover:shadow-blue-500/50 hover:bg-blue-500 font-semibold ">Get started 👉</button>
+    </div>
+</body>
+</html>
+
