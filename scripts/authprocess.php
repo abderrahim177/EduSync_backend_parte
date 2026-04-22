5073581f21d1e3ec86da6eb56a8685dcbaf7c906
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-04-22 10:33:02 +0100

    terminer la partie de register

---
+++ scripts/authprocess.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+require_once(__DIR__ . '/../includes/db.php');
+require_once "../includes/functions.php";
+
+if (isset($_POST['register'])) {
+
+    $nom = cleanInput($_POST['nom']);
+    $prenom = cleanInput($_POST['prenom']);
+    $email = cleanInput($_POST['email']);
+    $password = $_POST['password'];
+
+    // Validation
+    if (empty($nom) || empty($prenom) || empty($email) || empty($password)) {
+        header("Location: ../public/register.php?error=empty");
+        exit();
+    }
+
+    if (!isValidEmail($email)) {
+        header("Location: ../public/register.php?error=email");
+        exit();
+    }
+    if ($password !== $_POST['passwordRepeat']) {
+    header("Location: ../public/register.php?error=password");
+    exit();
+}
+    // Hash password 🔐
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert DB
+    $sql = "INSERT INTO users (nom, prenom, email, password) VALUES (?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$nom, $prenom, $email, $hashedPassword]);
+
+    header("Location: ../public/login.php?success=1");
+}
+
+if (isset($_POST['login'])) {
+
+    $email = cleanInput($_POST['email']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+
+        $_SESSION['user'] = [
+            'nom' => $user['nom'],
+            'role' => $user['role']
+        ];
+
+        header("Location: ../public/dashboard.php");
+    } else {
+        header("Location: ../public/login.php?error=invalid");
+    }
+}
