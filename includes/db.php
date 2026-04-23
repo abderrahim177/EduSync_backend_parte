7069cd28c87ca00658df13c45d458fe9b019fbd3
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-04-23 10:30:51 +0100

    Prevent duplicate users

--- includes/db.php
+++ includes/db.php
@@ -8,5 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur DB : " . $e->getMessage());
+    die("Erreur de connexion à la base de données.");
 }
+
